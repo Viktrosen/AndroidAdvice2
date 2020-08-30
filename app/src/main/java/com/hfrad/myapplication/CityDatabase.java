60279a1e4b207eb783275af6e6c25a9aa4c6381d
Viktrosen <bolotov.vit@yandex.ru>
2020-08-30 18:59:25 +0500

    Вроде готово

--- app/src/main/java/com/hfrad/myapplication/CityDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.hfrad.myapplication;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {City.class}, version = 1)
-public abstract class CityDatabase extends RoomDatabase {
-    public abstract CitiesDao getCityDao();
-}
